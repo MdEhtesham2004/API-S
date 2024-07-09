2d1e3bfe5aefe4afdcd927d90e44e2ab23790cc8
MohammedEhtesham2004 <ehteshammd089@gmail.com>
2024-07-09 16:55:24 +0530

    initial commit

---
+++ pushup_logger/README.md
@@ -0,0 +1,5 @@
+GET - method of requesting something from server for example refreshing the browser 
+POST - method of sending information to the server  or we can say returning something to the server such as user name and passwords 
+
+targeting html form elements using there names
+
---
+++ pushup_logger/auth.py
@@ -0,0 +1,68 @@
+from flask import Blueprint,render_template,url_for,request,redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db 
+from flask_login import logout_user,login_user,login_required
+
+auth = Blueprint('auth',__name__)
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@auth.route('/signup',  methods=['POST'])
+def signup_post():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        error_message = f"User with email '{email}' already exists!"
+        return render_template('signup.html', error_message=error_message)
+
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='pbkdf2:sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+
+    return redirect(url_for('auth.login'))  
+
+
+@auth.route('/login',methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password,password=password):
+        redirect(url_for('auth.login'))
+
+    login_user(user,remember=remember)
+
+    @login_required
+    def logout():
+        logout_user()
+        return redirect(url_for('main.index'))
+
+    return redirect(url_for('main.profile'))
+
+
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@auth.route('/logout')
+def logout():
+    return render_template('index.html')
+
+     
+    
+
+
